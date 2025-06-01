f33495d3abb329423954e912831b4eda30a59f34
Fauthog <tky40660@gmail.com>
2025-06-01 11:26:18 +0700

    update to python script

--- CreateManifest.py
+++ CreateManifest.py
@@ -89,19 +89,13 @@ def main():
     windows_dictionary = createDictionary(windows_directory, url)
 
     android_directory = config["base"] + config["android"]    
-    android_dictionary = createDictionary(android_directory, url)
-   
+    android_dictionary = createDictionary(android_directory, url)   
 
     experiment_directory = config["base"] + config["experiment"]    
     experiment_dictionary = createDictionary(experiment_directory, url)
+    
     windows_dictionary.update(experiment_dictionary) 
     android_dictionary.update(experiment_dictionary) 
-
-    video180_directory = config["video180"]  
-    video180_dictionary = createDictionary(video180_directory, url)
-   
-
-
 
     
     # check if manifest exists
@@ -110,189 +104,164 @@ def main():
     # print(manifest_path)
     if os.path.exists(manifest_path):
         print("File exists.")
-        with open(manifest_path, 'r') as file:
-            manifest= json.load(file)
+        try:
+            os.remove(manifest_path)
+            print("File deleted successfully.", manifest_path)
+        except FileNotFoundError:
+            print("The file does not exist.")
+        except PermissionError:
+            print("You do not have permission to delete this file.")
+        except Exception as e:
+            print(f"An error occurred while deleting the file: {e}")       
 
-        update = False
+        
 
-        for entry in manifest["android"]:
-            if entry not in android_dictionary:
-                file_path=os.path.join(config["output"], manifest["android"][entry]["filename"])
-                try:
-                    os.remove(file_path)
-                    print("File deleted successfully.")
-                except FileNotFoundError:
-                    print("The file does not exist.")
-                except PermissionError:
-                    print("You do not have permission to delete this file.")
-                except Exception as e:
-                    print(f"An error occurred while deleting the file: {e}")
-                keys_to_delete.append(entry)
-            else:
-                if manifest["android"][entry]["hash"] != android_dictionary[entry]["hash"]:
-                    update=True
-                    version=int(manifest["android"][entry]["version"]) + 1
-                    android_dictionary[entry]["version"]=version
 
-                    file_path=os.path.join(config["output"], manifest["android"][entry]["filename"])
+    manifest={}
+    current_datetime = datetime.now().strftime('%d%m%Y_%H%M%S')
+    manifest["version"]="2.0." + current_datetime
+
+    manifest["android"]=android_dictionary 
+    manifest["windows"]=windows_dictionary 
+
+    for entry in manifest["android"]: 
+        entry_path = os.path.join(config["output"], entry)
+        if os.path.exists(entry_path):
+            if os.path.getsize(entry_path) == manifest["android"][entry]["size"]:
+                hasher = hashlib.sha256()
+                with open(entry_path, 'rb') as f:
+                    chunk_size = 65536  # Read the file in chunks of 64KB
+                    while True:
+                        data = f.read(chunk_size)
+                        if not data:
+                            break
+                        hasher.update(data)
+                hash = hasher.hexdigest()
+                # print("hash", hash, manifest["android"][entry]["hash"], entry)
+                if hash == manifest["android"][entry]["hash"]:
+                    continue
+                    # print("same hash")
+                else:
+                    # print("different hash")
                     try:
-                        os.remove(file_path)
-                        print("File deleted successfully.")
+                        os.remove(entry_path)
+                        print("File deleted successfully.", entry_path)
                     except FileNotFoundError:
                         print("The file does not exist.")
                     except PermissionError:
                         print("You do not have permission to delete this file.")
                     except Exception as e:
                         print(f"An error occurred while deleting the file: {e}")
-                    manifest["android"][entry]=android_dictionary[entry]
-
-        for entry in android_dictionary:
-            if entry not in manifest["android"]:
-                manifest["android"][entry]=android_dictionary[entry]
-                update = True
-
-
-        for entry in manifest["windows"]:
-            if entry not in windows_dictionary:
-                file_path=os.path.join(config["output"], manifest["windows"][entry]["filename"])
-                try:
-                    os.remove(file_path)
-                    print("File deleted successfully.")
-                except FileNotFoundError:
-                    print("The file does not exist.")
-                except PermissionError:
-                    print("You do not have permission to delete this file.")
-                except Exception as e:
-                    print(f"An error occurred while deleting the file: {e}")
-                    keys_to_delete.append(entry)
-            else:
-                if manifest["windows"][entry]["hash"] != windows_dictionary[entry]["hash"]:
-                    update=True
-                    version=int(manifest["windows"][entry]["version"]) + 1
-                    windows_dictionary[entry]["version"]=version
-
-                    file_path=os.path.join(config["output"], manifest["windows"][entry]["filename"])
-                    try:
-                        os.remove(file_path)
-                        print("File deleted successfully.")
-                    except FileNotFoundError:
-                        print("The file does not exist.")
-                    except PermissionError:
-                        print("You do not have permission to delete this file.")
-                    except Exception as e:
-                        print(f"An error occurred while deleting the file: {e}")
-                    manifest["windows"][entry]=windows_dictionary[entry]
-
-        for entry in windows_dictionary:
-            if entry not in manifest["windows"]:
-                manifest["windows"][entry]=windows_dictionary[entry]
-                update = True
-        
-        for entry in manifest["video180"]:
-            if entry not in video180_dictionary:
-                file_path=os.path.join(config["output"], manifest["video180"][entry]["filename"])
-                try:
-                    os.remove(file_path)
-                    print("File deleted successfully1.")
-                except FileNotFoundError:
-                    print("The file does not exist.")
-                except PermissionError:
-                    print("You do not have permission to delete this file.")
-                except Exception as e:
-                    print(f"An error occurred while deleting the file: {e}")
-                    keys_to_delete.append(entry)
-            else:
-                if manifest["video180"][entry]["hash"] != video180_dictionary[entry]["hash"]:
-                    update=True
-                    version=int(manifest["video180"][entry]["version"]) + 1
-                    windows_dictionary[entry]["version"]=version
-
-                    file_path=os.path.join(config["output"], manifest["video180"][entry]["filename"])
-                    try:
-                        os.remove(file_path)
-                        print("File deleted successfully2.")
-                    except FileNotFoundError:
-                        print("The file does not exist.")
-                    except PermissionError:
-                        print("You do not have permission to delete this file.")
-                    except Exception as e:
-                        print(f"An error occurred while deleting the file: {e}")
-                    manifest["video180"][entry]=windows_dictionary[entry]
-        
-        for entry in video180_dictionary:
-            if entry not in manifest["video180"]:
-                manifest["video180"][entry]=windows_dictionary[entry]
-                update = True
-
-        if keys_to_delete is not None:
-            update=True
-            for key in keys_to_delete:
-                if key in manifest["android"]:
-                    manifest["android"].pop(key)
-                if key in manifest["windows"]:
-                    manifest["windows"].pop(key)
-
-        if update:
-            current_datetime = datetime.now().strftime('%d%m%Y_%H%M%S')
-            manifest["version"] = "2.0." + current_datetime
-
-        
-            for entry in manifest["android"]:            
-                if "path" in manifest["android"][entry]:
-                    copy_file(manifest["android"][entry]["path"], config["output"])
-                    manifest["android"][entry].pop("path")
-
-            for entry in manifest["windows"]:            
-                if "path" in manifest["windows"][entry]:
-                    copy_file(manifest["windows"][entry]["path"], config["output"])
-                    manifest["windows"][entry].pop("path")
-
-            for entry in manifest["video180"]:            
-                if "path" in manifest["video180"][entry]:
-                    copy_file(manifest["video180"][entry]["path"], config["output"])
-                    manifest["video180"][entry].pop("path")
                 
-            with open(manifest_path, 'w') as file:
-                json.dump(manifest, file)
-        
+                    copy_file(manifest["android"][entry]["path"], config["output"])
+
+            else:
+                # print("different size")
+                try:
+                    os.remove(entry_path)
+                    print("File deleted successfully.", entry_path)
+                except FileNotFoundError:
+                    print("The file does not exist.")
+                except PermissionError:
+                    print("You do not have permission to delete this file.")
+                except Exception as e:
+                    print(f"An error occurred while deleting the file: {e}")
+                
+                copy_file(manifest["android"][entry]["path"], config["output"])
         else:
-            print("no update to manifest")
-
-    else:
-        print("File does not exist.")
-        manifest={}
-        manifest["version"]="2.0.0"
-        manifest["android"]=android_dictionary   
-        # manifest["android"].update(experiment_dictionary) 
-        # print(manifest)
-        for entry in manifest["android"]: 
             copy_file(manifest["android"][entry]["path"], config["output"])
-            manifest["android"][entry].pop("path")
-            manifest["android"][entry]["version"]="1"
 
-        
-        manifest["windows"]=windows_dictionary
-        # manifest["windows"].update(experiment_dictionary)    
-        for entry in manifest["windows"]:            
-            if "path" in manifest["windows"][entry]:
+   
+    for entry in manifest["windows"]:        
+        entry_path = os.path.join(config["output"], entry)
+        if os.path.exists(entry_path):
+            if os.path.getsize(entry_path) == manifest["windows"][entry]["size"]:
+                hasher = hashlib.sha256()
+
+                with open(entry_path, 'rb') as f:
+                    chunk_size = 65536  # Read the file in chunks of 64KB
+                    while True:
+                        data = f.read(chunk_size)
+                        if not data:
+                            break
+                        hasher.update(data)
+                hash = hasher.hexdigest()
+                # print("hash", hash, manifest["windows"][entry]["hash"], entry)
+                if hash == manifest["windows"][entry]["hash"]:
+                    # print("same hash")
+                    continue
+                else:
+                    # print("different hash")
+                    try:
+                        os.remove(entry_path)
+                        print("File deleted successfully.", entry_path)
+                    except FileNotFoundError:
+                        print("The file does not exist.")
+                    except PermissionError:
+                        print("You do not have permission to delete this file.")
+                    except Exception as e:
+                        print(f"An error occurred while deleting the file: {e}")
+                
+                    copy_file(manifest["windows"][entry]["path"], config["output"])
+
+            else:
+                # print("different size")
+                try:
+                    os.remove(entry_path)
+                    print("File deleted successfully.", entry_path)
+                except FileNotFoundError:
+                    print("The file does not exist.")
+                except PermissionError:
+                    print("You do not have permission to delete this file.")
+                except Exception as e:
+                    print(f"An error occurred while deleting the file: {e}")
+                
                 copy_file(manifest["windows"][entry]["path"], config["output"])
-                manifest["windows"][entry].pop("path")
-            manifest["windows"][entry]["version"]="1"
+        else:
+            copy_file(manifest["windows"][entry]["path"], config["output"])
 
-        manifest["video180"]=video180_dictionary
-        for entry in manifest["video180"]:
-            if "path" in manifest["video180"][entry]:
-                copy_file(manifest["video180"][entry]["path"], config["output"])
-                manifest["video180"][entry].pop("path")
-            manifest["video180"][entry]["version"]="1"
+    
+    for entry in manifest["android"]:
+        if "path" in manifest["android"][entry]:
+            # print("deleted path")
+            manifest["android"][entry].pop("path")     
+
+    for entry in manifest["windows"]:
+        if "path" in manifest["windows"][entry]:
+            # print("deleted path")
+            manifest["windows"][entry].pop("path")
+    
+
+#    cleaning up not used files
+    output_dictionary = createDictionary(config["output"], url)
+    for entry in output_dictionary:
+        if (entry in manifest["android"]) or (entry in manifest["windows"]):
+            continue
+        else:
+            entry_path = os.path.join(config["output"], entry)
+            print("cleanup", entry)
+            try:
+                os.remove(entry_path)
+                print("File deleted successfully.", entry_path)
+            except FileNotFoundError:
+                    print("The file does not exist.")
+            except PermissionError:
+                    print("You do not have permission to delete this file.")
+            except Exception as e:
+                    print(f"An error occurred while deleting the file: {e}")
+
+      
+
+
 
         
         
-        with open(manifest_path, 'w') as file:
-            json.dump(manifest, file)
+    with open(manifest_path, 'w') as file:
+        json.dump(manifest, file)
+        print("Manifest written")
 
 
-  
+    
 
 
 if __name__ == "__main__":
